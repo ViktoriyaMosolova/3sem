003afd93791b9b207858feca96c633a46dd76f00
unknown <mosolova.viktoriya@mail.ru>
2022-10-22 20:27:40 +0300

    22 окт 2022 г. 20:27:40

---
+++ 21/stack.c
@@ -0,0 +1,12 @@
+#define STACK_MAX_SIZE 20
+typedef int T;
+
+typedef struct Stack_tag {
+    T data[STACK_MAX_SIZE];
+    size_t size;
+} Stack_t;
+
+void push(Stack_t *stack, const T value) {
+    stack->data[stack->size] = value;
+    stack->size++;
+}
