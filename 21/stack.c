ec5cf1af9be50d6d5e10348af944e70d05b61222
unknown <mosolova.viktoriya@mail.ru>
2022-10-23 14:18:18 +0300

    23 окт 2022 г. 14:18:18

--- 21/stack.c
+++ 21/stack.c
@@ -1,12 +1,32 @@
-#define STACK_MAX_SIZE 20
-typedef int T;
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Stack_tag {
-    T data[STACK_MAX_SIZE];
-    int size;
-} Stack_t;
+typedef struct list{
+    int data;
+    struct list *next;
+} t_list;
 
-void push(Stack_t *stack, const T value) {
-    stack->data[stack->size] = value;
-    stack->size++;
+t_list *create_element(int data) {
+    t_list *element = (t_list*)malloc(sizeof(t_list));
+    element -> data = data;
+    element -> next = NULL;
+    return element;
+}
+
+void add_element(t_list **l, int data) {
+    t_list *add_element = create_element(data);
+    add_element -> next = *l;
+    *l = add_element;
+}
+
+
+
+int main() {
+    t_list *l = create_element(1);
+    add_element(&l, 2);
+    while(l != NULL) {
+        printf("data = %d\n", l -> data);
+        l = l -> next;
+    }
+    return 0;
 }
