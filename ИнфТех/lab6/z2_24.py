4ffc966142cee05a8826808b12e968a80b7c2006
unknown <mosolova.viktoriya@mail.ru>
2022-12-09 19:35:16 +0300

    9 дек 2022 г. 19:35:15

---
+++ ИнфТех/lab6/z2_24.py
@@ -0,0 +1,14 @@
+'''7.2 Библиотека Numpy. Действия над массивами (Z2) 
+24. Создать массив 2*3 заполненный случайными целыми числами от 23 до 69. 
+Транспонировав массив,
+отнять от каждого элемента массива 17 и вставить после 1-й позиции столбец [0, 0]. '''
+
+import numpy as n
+a = n.random.randint(23,70,(2,3),dtype=int) #interval [low, high)
+print(f'a:\n{a}\n')
+a=n.transpose(a)
+print(f'a_transpose:\n{a}\n')
+a-=17
+print(f'a=a-17:\n{a}\n')
+a=n.insert(a, 1, [0,0], axis=0)
+print(f'add[0,0]:\n{a}\n')
