bb05450626b615f2b8b5c1ff7ad227a9591333e0
unknown <mosolova.viktoriya@mail.ru>
2022-12-14 13:54:52 +0300

    14 дек 2022 г. 13:54:51

--- ИнфТех/lab5/lab6_18.1.py
+++ ИнфТех/lab5/lab6_18.1.py
@@ -13,45 +13,138 @@
 
  Перехват ошибок расчёта (возможные арифметические ошибки и пр.). '''
 
-from math import exp, pow
+from math import pow
 
-def computation():
+def computation(t, sv):
     try:
         x = sv / (1900 - 18 * sv)
-        y = exp(22.46 * x - 0.114 + ((30 - t) / (91 + t)) * (1.1 + 43.1 * pow(x, 1.25)))
+        y = pow(10, (22.46 * x - 0.114 + ((30 - t) / (91 + t)) * (1.1 + 43.1 * pow(x, 1.25))))
         return y
     except ZeroDivisionError:
         print("Деление на 0")
+        exit(1)
 
+##для считывания с консоли
 try:
-    p=input("Введите диапазон p и шаг через пробел(a b step): ")
-    sv=input("Введите диапазон СВ и шаг через пробел(a b step): ")
-    p=list(map(float, p.split(sep=" ")))
+    t=input("Введите диапазон t(oC) и шаг через пробел(a b step): ")
+    sv=input("Введите диапазон СВ(%) и шаг через пробел(a b step): ")
+    t=list(map(float, t.split(sep=" ")))
     sv=list(map(float, sv.split(sep=" ")))
 except ValueError:
-    print("Введено неправильное значение\nВведите значения p и СВ таким образом:\n"
-        "диапазон p и шаг(a b step): 5 42 2\nдиапазон СВ и шаг(a b step): 5 60 5\n")
+    print("input_console:Не те значения!!Нужно ввести диапазон и шаг для численных параметров через пробел(a b step)!!!\n")
     exit(1)
 except KeyboardInterrupt:
-    print("Введено неправильное значение\nВведите значения p и СВ таким образом:\n"
-            "диапазон p и шаг(a b step):\n5 42 2\nдиапазон СВ и шаг(a b step):\n5 60 5\n")
+    print("input_console:Не нажимайте клавиши для прерывания процесса!!\n")
+    exit(1)
+except EOFError:
+    print("input_console:Неожиданный символ конца файла!!\n")
     exit(1)
 
-step_p=int((p[1]-p[0])/p[2])
-step_sv=int((sv[1]-sv[0])/sv[2])
-p0=p[0]
-sv0=sv[0]
-if len(sv)==len(p)==3:
-    with open("output_console.txt", "w") as f:
-        for i in range(step_p):
-            p0=p[0]+i*step_p
-            for j in range(step_sv):
-                sv0=sv[0]+j*step_sv
-                f.write(f"p(oC)={p0:<8}sv(%)={sv0:<8}n(сП)={computation(p0, sv0):<8.8}\n")
-    f.close()
-else:
-    print("Нужно ввести диапазон и шаг для p и СВ!!\nтаким образом:\n"
-    "диапазон p и шаг(a b step):\n5 42 2\nдиапазон СВ и шаг(a b step):\n5 60 5\n")
+if (len(sv)!=3 or len(t)!=3):
+    print("input_console:Нужно ввести по 3 значения для каждого параметра. "
+        "Диапазон и шаг через пробел(a b step)\n")
     exit(1)
+
+flag=0
+if ((t[0] or t[1]) < -273.15):
+    print("input_console:Температура не может быть менее -273.15oC\n")
+    flag=1
+if ((sv[0] or sv[1]) < 0) or ((sv[0] or sv[1]) > 100):
+    print("input_console:Значение концентрации не может быть более 100% или менее 0%\n")
+    flag=1
+if flag==1: 
+    exit(1)
+
+try:
+    step_t=int((t[1]-t[0])/t[2])
+    step_sv=int((sv[1]-sv[0])/sv[2])
+    t0=t[0]
+    sv0=sv[0]
+except ZeroDivisionError:
+    print("input_console:Шаг не может быть 0")
+    exit(1)
+
+try:
+    for i in range(step_t+1):
+        t0=t[0]+i*t[2]
+        for j in range(step_sv+1):
+            sv0=sv[0]+j*sv[2]
+            print(f"t(oC)={t0:<8}sv(%)={sv0:<8}n(sP)={computation(t0, sv0):<8.3}")
+    dt=[40,50,30]
+    dsv=[0,50,30]
+    n_real=[0.65,4.94,2.5]
+    for i in range(3):
+        n_comp=computation(dt[i], dsv[i])
+        dn=abs((n_real[i] - n_comp) * 100)
+        print(f"t(oC)={dt[i]:<8}sv(%)={dsv[i]:<8}n_real(sP)={n_real[i]:<8.3}n_comp(sP)={n_comp:<8.3}n_error(%)={dn:<8.3}")
+except ValueError:
+    print("input_console:Не может быть вычислена функция")
+    exit(1)
+except OverflowError:
+    print("input_console:Слишком большой результат")
+    exit(1)
+
+
+##для считывания с файла
+try:
+    with open("input_file.txt", "r") as d:
+        t_f=list(map(float, d.readline().split(sep=" "))) 
+        sv_f=list(map(float, d.readline().split(sep=" "))) 
+    d.close()
+except ValueError:
+    print("input_file:Не те значения!!Нужно ввести диапазон и шаг для численных параметров через пробел(a b step)!!!\n")
+    exit(1)
+except FileNotFoundError:
+    print("input_file:Не удалось найти файл")
+    exit(1)
+except EOFError:
+    print("input_file:Неожиданный символ конца файла!!\n")
+    exit(1)
+
+if (len(sv_f)!=3 or len(t_f)!=3):
+    print("input_file:Нужно ввести по 3 значения для каждого параметра. "
+        "Диапазон и шаг через пробел(a b step)\n")
+    exit(1)
+
+flag=0
+if ((t_f[0] or t_f[1]) < -273.15):
+    print("input_file:Температура не может быть менее -273.15oC\n")
+    flag=1
+if ((sv_f[0] or sv_f[1]) < 0) or ((sv_f[0] or sv_f[1]) > 100):
+    print("input_file:Значение концентрации не может быть более 100% или менее 0%\n")
+    flag=1
+if flag==1: 
+    exit(1)
+
+try:
+    step_t_f=int((t_f[1]-t_f[0])/t_f[2])
+    step_sv_f=int((sv_f[1]-sv_f[0])/sv_f[2])
+    t0_f=t_f[0]
+    sv0_f=sv_f[0]
+except ZeroDivisionError:
+    print("input_file:Шаг не может быть 0")
+    exit(1)
+
+with open("output_file.txt", "w") as f:
+    try:
+        for i in range(step_t_f+1):
+            t0_f=t_f[0]+i*t_f[2]
+            for j in range(step_sv_f+1):
+                sv0_f=sv_f[0]+j*sv_f[2]
+                f.write(f"t(oC)={t0_f:<8}sv(%)={sv0_f:<8}n(sP)={computation(t0_f, sv0_f):<8.3}\n")
+        dt_f=[40,50,30]
+        dsv_f=[0,50,30]
+        n_real_f=[0.65,4.94,2.5]
+        for i in range(3):
+            n_comp_f=computation(dt_f[i], dsv_f[i])
+            dn_f=abs((n_real_f[i] - n_comp_f) * 100)
+            f.write(f"t(oC)={dt_f[i]:<8}sv(%)={dsv_f[i]:<8}n_real(sP)={n_real_f[i]:<8.3}n_comp(sP)={n_comp_f:<8.3}n_error(%)={dn_f:<8.3}\n")
+    except ValueError:
+        print("input_file:Не может быть вычислена функция")
+        exit(1)
+    except OverflowError:
+        print("input_file:Слишком большой результат")
+        exit(1)
+f.close()
 
 
